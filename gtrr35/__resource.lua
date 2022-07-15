954f2db297eca15d0490681f9074919fd51a67c5
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-07-16 01:47:43 +0545

    Add files via upload

---
+++ gtrr35/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'shop_vechile.meta',
+    'handling.meta',
+    'gtr35kreamccontentunlocks.meta',
+	'dlctext.meta',    -- Not Required
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'SHOP_FILE' 'shop_vechile'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
+data_file 'DLC_TEXT' 'dlctext.meta'
+
