1de5b3fa87e5f2fbe2fb0b9337ab2f0d1789c158
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-07-16 02:06:45 +0545

    Add files via upload

---
+++ toyotasupra/__resource.lua
@@ -0,0 +1,24 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'audioconfig/toysupmk4_game.dat151.nametable',
+    'audioconfig/toysupmk4_game.dat151.rel',
+    'audioconfig/toysupmk4_sounds.dat54.nametable',
+    'audioconfig/toysupmk4_sounds.dat54.rel',
+    'sfx/dlc_toysupmk4/toysupmk4.awc',
+    'sfx/dlc_toysupmk4/toysupmk4_npc.awc'
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.META'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/toysupmk4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/toysupmk4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_toysupmk4'
