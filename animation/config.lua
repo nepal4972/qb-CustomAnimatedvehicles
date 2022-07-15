44c9b63fd59a7c9d8bc7d3d6b522cc4cceb7700d
Saugat Nepal <83812124+nepal4972@users.noreply.github.com>
2022-07-16 01:45:38 +0545

    Add files via upload

---
+++ animation/config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+Config.cars = {
+    {
+        ["Model"] = "rmodsian",     --Vehicle Spawn Name
+        ["Texture"] = "rmodsian_sign_1",   --Texture Name
+        ["Image"] = "https://cdn.discordapp.com/attachments/799977755314749451/834850042153664553/giphy_2.gif",     --Animated Livery
+        ["Loaded"] = false  --Don't touch this field
+    },
+    {
+        ["Model"] = "rmodsian",
+        ["Texture"] = "rmodsian_sign_2",
+        ["Image"] = "https://cdn.discordapp.com/attachments/799977755314749451/834850042153664553/giphy_2.gif",
+        ["Loaded"] = false
+    },
+}
---
+++ animation/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games { 'gta5' };
+
+client_script {
+	'config.lua',
+	'client.lua',
+}
+
+server_script {
+	'server.lua',
+}
